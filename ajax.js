4b6d41c4e7bd1fd9044a5925bfe77fb5fc9b6c95
marcusmarcelino <marcusmarcelinosilva1010@gmail.com>
2019-11-06 17:21:44 -0300

    Correção de bugs do dataTable executada, inserção da chamada no local correto

--- ajax.js
+++ ajax.js
@@ -1,19 +1,30 @@
 $(document).ready(function () {
-   $('#tabelaDeEventos').DataTable();
-   $('.dataTables_length').addClass('bs-select');
-
-   getList();
+   getList();  
 });
 
-function getList (){
+function getList() {
    $.ajax({
       url: 'controller.php?op=getList',
       method: 'GET',
       dataType: 'text',
-      success: function(response){}
-   }).done(function(response){
-      $('#tbody').html(response);
-   }).fail(function(error){
+      success: function (response) { }
+   }).done(function (response) {
+      if(response != "BaseDeDadosVazia"){
+         $('#tbody').html(response);
+         $('#tabelaDeEventos').DataTable({
+            "language": {
+               "lengthMenu": "Mostrando _MENU_ registros por página",
+               "zeroRecords": "Nada encontrado",
+               "info": "Mostrando página _PAGE_ de _PAGES_",
+               "infoEmpty": "Nenhum registro disponível",
+               "infoFiltered": "(filtrado de _MAX_ registros no total)"
+            },
+            "order": [[ 3, "desc" ]]
+         });
+         $('.dataTables_length').addClass('bs-select');
+      }
+      
+   }).fail(function (error) {
       console.log(error);
    });
 }
