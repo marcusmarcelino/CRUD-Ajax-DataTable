ca797f06a43f434875509e7a98cd0749443a2143
marcusmarcelino <marcusmarcelinosilva1010@gmail.com>
2019-11-05 21:58:43 -0300

    Função getList() implementada!

---
+++ controller.php
@@ -0,0 +1,53 @@
+<?php
+$op = $_GET['op'];
+switch ($op){
+   case 'getList':
+      getList();
+      break;
+   case 'save':
+      save();
+      break;
+   case 'edit':
+      edit();
+      break;
+   case 'delet':
+      delet();
+      break;
+   default:
+      echo "Entrou na opção default";
+      break;
+}
+
+function getList (){
+   include_once("conexao.php");
+   if(isset($_GET)){
+      $sql = "SELECT * FROM events";
+      $result = mysqli_query($conn,$sql);
+      $response = '';
+
+      if($result->num_rows > 0){
+         while($data = $result->fetch_assoc()){
+            $response = '
+               <tr>
+                  <td scope="row" id="event_'.$data['id'].'">'.$data['nome'].'</td>
+                  <td scope="row">'.$data['local'].'</td>
+                  <td scope="row">'.$data['data'].'</td>
+                  <td>
+                     <button value="edit" onclick="edit('.$data['id'].')" type="button" name="edit" id="edit" class="btn btn-warning">
+                        <i class="fa fa-edit"></i></button>
+                     <button value="view" onclick="view('.$data['id'].')" type="button" name="edit" id="edit" class="btn btn-primary">
+                        <i class="fa fa-info" aria-hidden="true"></i>
+                     </button>
+                     <button value="delet" onclick="delet('.$data['id'].')" type="button" name="edit" id="edit" class="btn btn-danger">
+                        <i class="fa fa-trash" aria-hidden="true"></i>
+                     </button>
+                  </td>
+               </tr>
+            ';
+         }exit($response);
+      }else{
+         exit("A base de dados está vazia");
+      }
+   }
+   mysqli_close($conn);
+}
